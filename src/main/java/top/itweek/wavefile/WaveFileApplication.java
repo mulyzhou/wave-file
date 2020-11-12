4535ac72bdd50abba52c05be2b0813d6cdcf5b90
mulyzhou <muly_zhou@163.com>
2020-11-12 14:02:14 +0800

    init commit

---
+++ src/main/java/top/itweek/wavefile/WaveFileApplication.java
@@ -0,0 +1,13 @@
+package top.itweek.wavefile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaveFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaveFileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/itweek/wavefile/WaveFileApplicationTests.java
@@ -0,0 +1,13 @@
+package top.itweek.wavefile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaveFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
